13277d2f12e19570a68e19da6ee38b4ce72614ce
Kimo1928 <kareemmohamed88813@gmail.com>
2025-10-23 11:03:56 +0300

    EndOfSection3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, signal, Signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'DatingApp';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users=signal<any>([]);
+  // private http: HttpClient;
+  constructor(private http:HttpClient) {}
+
+
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response =>this.users.set(response) ,
+      error: error => console.log(error) ,
+      complete: () => console.log('Request completed')
+    });
+  }
 }
